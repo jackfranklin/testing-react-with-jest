57a372d7a8053122df69f4f139a0dd4b531956be
Jack Franklin <jack@jackfranklin.net>
2020-10-19 19:20:48 +0100

    Upgrade everything to latest

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'babel-loader'
     }]
   }
 }
